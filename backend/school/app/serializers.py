ba2e4af49c2876d120a03cfcbd595e4dbd49a9af
alinrmkh <amanbekovaalina10@gmail.com>
2023-11-28 21:20:57 +0600

    serializers

--- backend/school/app/serializers.py
+++ backend/school/app/serializers.py
@@ -12,3 +12,50 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+        
+        from rest_framework import serializers
+from .models import Class, Subject, UserAccount, Teacher, Student, Applicant, Grade, News
+
+class ClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Class
+        fields = '__all__'
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = '__all__'
+
+class UserAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserAccount
+        fields = '__all__'
+
+class TeacherSerializer(serializers.ModelSerializer):
+    subjects_taught = SubjectSerializer(many=True)
+
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+
+class StudentSerializer(serializers.ModelSerializer):
+    class_enrolled = ClassSerializer()
+
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+class ApplicantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Applicant
+        fields = '__all__'
+
+class GradeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Grade
+        fields = '__all__'
+
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = '__all__'
