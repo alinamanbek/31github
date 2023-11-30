d7c681951264842f2ebb434df3575c54733976e6
alinrmkh <amanbekovaalina10@gmail.com>
2023-11-30 21:43:05 +0600

    serializers

--- backend/school/urls.py
+++ backend/school/urls.py
@@ -25,7 +25,7 @@ from school.app import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+#router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
